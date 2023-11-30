408faa96bc43165f6f0de683ea205bfc9e4b25ee
ZiVan21Ft <90625862+ZiVan21Ft@users.noreply.github.com>
2023-11-30 09:22:20 +0630

    Create name.js

---
+++ name.js
@@ -0,0 +1,25 @@
+function greet(a,b,c){
+  //sum ab
+  var sum = parseInt(a, 10);
+  var suma = (sum/58) * 60;
+
+  //var sum = parseInt(a, 10) + parseInt(b, 10);
+  //var asr = (sum/100) * 120;
+
+  //contain.textContent = "The result is"+""+ sum;
+  //sum c
+  var sumc = parseInt(c, 10);
+  var asrc = (sumc/62) * 60;
+  const total_num = Math.trunc(suma) + parseInt(b, 10) + Math.trunc(asrc);
+  document.getElementById("contain").innerText = "Your's total mark is"+ " "+ total_num+".";
+  //alert(sum);
+  //document.write(sum);
+  //document.write("hello"+firstname+""+secondname);
+}
+function isInputNumber(e) {
+  var x = e.which || e.keycode;
+  if((x >= 48 && x <= 57))
+    return true;
+  else
+    return false;
+}
